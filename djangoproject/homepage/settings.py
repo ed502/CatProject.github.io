2f40f2110d0b706ee5102d99f574480c36e34550
이세민 <sky74zkr@gmail.com>
2019-07-29 20:19:56 +0900

    수정중

--- djangoproject/homepage/settings.py
+++ djangoproject/homepage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ho944x!jes^@2c_52hcgegmgxnqs1o
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
